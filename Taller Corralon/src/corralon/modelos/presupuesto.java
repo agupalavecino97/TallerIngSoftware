6bfe5d7a724f6e844d39a6ce7d135ff27da78110
Unknown <exequielpardoxerex@gmail.com>
2018-09-19 09:54:18 -0300

    Clase presupuesto creada.

--- Taller Corralon/src/corralon/modelos/presupuesto.java
+++ Taller Corralon/src/corralon/modelos/presupuesto.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package corralon.modelos;
 
+package corralon.modelos;
+import java.sql.Date;
 /**
  *
  * @author Agustin
  */
 public class presupuesto {
+    
+    private int codigoPedidoClie;
+    private int dniCliente;
+    private Date fechaPresupuesto;
+
+    public presupuesto(int codigoPedidoClie, int dniCliente, Date fechaPresupuesto) {
+        this.codigoPedidoClie = codigoPedidoClie;
+        this.dniCliente = dniCliente;
+        this.fechaPresupuesto = fechaPresupuesto;
+    }
+
+    public int getCodigoPedidoClie() {
+        return codigoPedidoClie;
+    }
+
+    public void setCodigoPedidoClie(int codigoPedidoClie) {
+        this.codigoPedidoClie = codigoPedidoClie;
+    }
+
+    public int getDniCliente() {
+        return dniCliente;
+    }
+
+    public void setDniCliente(int dniCliente) {
+        this.dniCliente = dniCliente;
+    }
+
+    public Date getFechaPresupuesto() {
+        return fechaPresupuesto;
+    }
+
+    public void setFechaPresupuesto(Date fechaPresupuesto) {
+        this.fechaPresupuesto = fechaPresupuesto;
+    }
+    
     
 }
