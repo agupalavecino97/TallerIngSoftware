8bae1dbec4897ff4659a9ec5f79f044fe3977dd9
Unknown <maxi.orellano@gmail.com>
2018-09-21 22:33:16 -0300

    en la clase clienteDAO, en lugar de utilizar Long, no deberiamos usar int? recordar que nuestra clase cliente usa Int y no Long, como el ejemplo.

--- Taller Corralon/src/corralon/DAO/clienteDAO.java
+++ Taller Corralon/src/corralon/DAO/clienteDAO.java
@@ -1,6 +1,7 @@
 package corralon.DAO;
 import corralon.modelos.cliente;
 
+//En nuestro caso no iria en lugar de un long un int? en el video muestra que utiliza long para la PK, pero nosotros usamos un int
 public interface clienteDAO extends DAO<cliente, Long>{
      
 }
