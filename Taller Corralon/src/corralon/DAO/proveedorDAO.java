a4f336e77d0ebd9cb95daf118d0f81c54b1f5b17
Unknown <maxi.orellano@gmail.com>
2018-09-22 19:19:01 -0300

    clase proveedorDAO creada

---
+++ Taller Corralon/src/corralon/DAO/proveedorDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corralon.DAO;
+import corralon.modelos.proveedor;
+/**
+ *
+ * @author ORELLANO
+ */
+public interface proveedorDAO extends DAO<proveedor, Long>{
+    
+}
