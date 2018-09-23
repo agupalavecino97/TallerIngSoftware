a63ea27324824b3e6d68cc5e0b70ea39f3e153e1
Unknown <agupalavecino97@gmail.com>
2018-09-23 16:04:31 -0300

    modelo producto incompleto

--- Taller Corralon/src/corralon/DAO/mysql/MySQLProveedorDAO.java
+++ Taller Corralon/src/corralon/DAO/mysql/MySQLProveedorDAO.java
@@ -3,7 +3,9 @@ package corralon.DAO.mysql;
 
 import corralon.DAO.proveedorDAO;
 import corralon.modelos.proveedor;
+import corralon.modelos.stock;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
