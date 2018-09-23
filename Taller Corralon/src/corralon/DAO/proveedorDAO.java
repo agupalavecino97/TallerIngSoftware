451b6cbc7332b47335e1df6ee9622a962a727f52
Unknown <agupalavecino97@gmail.com>
2018-09-23 12:15:54 -0300

    Cooreccion de unos errores que daba
    habia como unas lineas de codigo raras que daba error no se de donde salieron, si se generaron por el repositorio o que

--- Taller Corralon/src/corralon/DAO/proveedorDAO.java
+++ Taller Corralon/src/corralon/DAO/proveedorDAO.java
@@ -1,22 +1,8 @@
-<<<<<<< Updated upstream
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package corralon.DAO;
-import corralon.modelos.proveedor;
-/**
- *
- * @author ORELLANO
- */
-=======
-
 package corralon.DAO;
 
 import corralon.modelos.proveedor;
 
->>>>>>> Stashed changes
+
 public interface proveedorDAO extends DAO<proveedor, Long>{
     
 }
