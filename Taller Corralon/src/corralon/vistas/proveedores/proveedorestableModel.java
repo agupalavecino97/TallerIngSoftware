2ec7be49179e902a582a912b9082616e988faa20
Unknown <maxi.orellano@gmail.com>
2018-10-02 13:53:18 -0300

    clase proveedorestableModel

---
+++ Taller Corralon/src/corralon/vistas/proveedores/proveedorestableModel.java
@@ -0,0 +1,57 @@
+
+package corralon.vistas.proveedores;
+
+import corralon.DAO.proveedorDAO;
+import corralon.modelos.proveedor;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class proveedorestableModel extends AbstractTableModel{
+    private proveedorDAO proveedor;
+    private List<proveedor> datos=new ArrayList<>();
+    
+        public proveedorestableModel(proveedorDAO proveedor){
+        this.proveedor=proveedor;
+    }
+   
+    public void updateModel(){
+            datos=proveedor.obtenerTodos();
+    }
+    
+    public String getColumnName(int i) {
+        switch(i){
+            case 0:return "cuitCliente";
+            case 1:return "razonSocial";
+            case 2:return "Direccion";
+            case 3:return "Telefono";
+            case 4:return "Estado";
+            default:return "[no]";
+        }     
+    }
+
+
+    @Override
+    public int getRowCount() {
+        return datos.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columIndex) {
+       proveedor consulta=datos.get(rowIndex);
+       switch (columIndex){
+           case 0: return consulta.getCuitProveedor();
+           case 1: return consulta.getNombreProveedor();
+           case 2: return consulta.getDireccionProveedor();
+           case 3: return consulta.getTelefonoProveedor();
+           case 4: return consulta.isEstadoProveedor();
+           default: return "";
+       }     
+    }
+    
+}
