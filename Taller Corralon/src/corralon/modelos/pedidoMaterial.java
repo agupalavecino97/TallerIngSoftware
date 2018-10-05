52f9e5bf0d86b2b415bdbabb92976f3fae8c764c
Unknown <agupalavecino97@gmail.com>
2018-10-05 19:05:11 -0300

    Retiro de atributo innecesario
    este atributo no tendira que ir el id del objeto es solo el id del pedido del cliente, cabie los getter y setter y constructor

--- Taller Corralon/src/corralon/modelos/pedidoMaterial.java
+++ Taller Corralon/src/corralon/modelos/pedidoMaterial.java
@@ -4,13 +4,12 @@ package corralon.modelos;
 public class pedidoMaterial {
     
     private Long codigoPedidoClie;
-    private int codigoPedidoMaterial;
+    //private int codigoPedidoMaterial;   //AQUI este atributo no tendria que existir. LO SACO.
     private int cantidadMaterial;
     private float subTotalMat;
 
-    public pedidoMaterial(Long codigoPedidoClie, int codigoPedidoMaterial, int cantidadMaterial, float subTotalMat) {
+    public pedidoMaterial(Long codigoPedidoClie, int cantidadMaterial, float subTotalMat) {
         this.codigoPedidoClie = codigoPedidoClie;
-        this.codigoPedidoMaterial = codigoPedidoMaterial;
         this.cantidadMaterial = cantidadMaterial;
         this.subTotalMat = subTotalMat;
     }
@@ -19,37 +18,25 @@ public class pedidoMaterial {
         return codigoPedidoClie;
     }
 
-    public void setCodigoPedidoClie(Long codigoPedidoClie) {
-        this.codigoPedidoClie = codigoPedidoClie;
-    }
-
-    public int getCodigoPedidoMaterial() {
-        return codigoPedidoMaterial;
-    }
-
-    public void setCodigoPedidoMaterial(int codigoPedidoMaterial) {
-        this.codigoPedidoMaterial = codigoPedidoMaterial;
-    }
-
     public int getCantidadMaterial() {
         return cantidadMaterial;
-    }
-
-    public void setCantidadMaterial(int cantidadMaterial) {
-        this.cantidadMaterial = cantidadMaterial;
     }
 
     public float getSubTotalMat() {
         return subTotalMat;
     }
 
+    public void setCodigoPedidoClie(Long codigoPedidoClie) {
+        this.codigoPedidoClie = codigoPedidoClie;
+    }
+
+    public void setCantidadMaterial(int cantidadMaterial) {
+        this.cantidadMaterial = cantidadMaterial;
+    }
+
     public void setSubTotalMat(float subTotalMat) {
         this.subTotalMat = subTotalMat;
     }
 
-    @Override
-    public String toString() {
-        return "pedidoMaterial{" + "codigoPedidoClie=" + codigoPedidoClie + ", codigoPedidoMaterial=" + codigoPedidoMaterial + ", cantidadMaterial=" + cantidadMaterial + ", subTotalMat=" + subTotalMat + '}';
-    }
     
 }
