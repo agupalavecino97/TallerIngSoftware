cf41f52c4043736829b89295464ff7d858459e98
Unknown <maxi.orellano@gmail.com>
2018-10-08 02:51:38 -0300

    confirmar pedidoTableModel

--- Taller Corralon/src/corralon/vistas/pedidoProveedor/confirmarPedido.java
+++ Taller Corralon/src/corralon/vistas/pedidoProveedor/confirmarPedido.java
@@ -17,17 +17,73 @@ public class confirmarPedido extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        cancelar = new javax.swing.JButton();
+        continuar = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jLabel1.setText("jLabel1");
+
+        cancelar.setText("Cancelar");
+
+        continuar.setText("Confirmar Compra");
+
+        jLabel2.setText("Total de la compra");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(cancelar)
+                        .addGap(42, 42, 42)
+                        .addComponent(jLabel1)))
+                .addContainerGap(76, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(continuar)
+                .addGap(28, 28, 28))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1))
+                    .addComponent(cancelar))
+                .addGap(12, 12, 12)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 94, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(continuar)
+                    .addComponent(jLabel2))
+                .addContainerGap())
         );
 
         pack();
@@ -69,6 +125,12 @@ public class confirmarPedido extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cancelar;
+    private javax.swing.JButton continuar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 
 }
---
+++ Taller Corralon/src/corralon/vistas/pedidoProveedor/confirmarPedidoTableModel.java
@@ -0,0 +1,56 @@
+
+package corralon.vistas.pedidoProveedor;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+public class confirmarPedidoTableModel extends AbstractTableModel{
+    
+    
+        private List<elementosDePedido> datos=new ArrayList();
+    
+     public confirmarPedidoTableModel(List datos){
+        this.datos=datos;
+    }
+
+          
+        @Override
+    public String getColumnName(int i){
+        switch(i){
+            case 0:return "codigo";
+            case 1:return "nombre";
+            case 2:return "cantidad solicitada";
+            case 3:return "precio";
+            default:return "[no]"; 
+        }
+    }     
+     
+        @Override
+       public Object getValueAt(int rowIndex, int columnIndex) {
+        elementosDePedido consulta=datos.get(rowIndex);
+        switch(columnIndex){
+            case 0:return consulta.getCodProducto();
+            case 1:return consulta.getNombrePr();
+            case 2:return consulta.getPrecio();
+            case 3:return consulta.getCantidad();
+            default: return "";
+        }
+    }
+     
+     
+     
+     
+     
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+}
